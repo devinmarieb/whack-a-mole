ff16804cfa06b52c4986f36dfc6ab4cdbb1a0e03
Devin Beliveau <devinmarieb@gmail.com>
2017-08-17 13:48:39 -0600

    Add temp log to show Y

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ function reset(random) {
 
 moles.forEach((mole) => {
   mole.addEventListener('click', (e)=> {
+    console.log(e.screenY);
     if(e.screenY <= 363) {
       points.innerHTML++
       reset(mole);
