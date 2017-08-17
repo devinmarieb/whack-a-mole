c4251898d39ba722c25a9ee11c4efda9daf6cb56
Devin Beliveau <devinmarieb@gmail.com>
2017-08-17 13:13:52 -0600

    Add timer

--- main.js
+++ main.js
@@ -1,38 +1,43 @@
 const moles = document.querySelectorAll('.mole')
-const startBtn = document.getElementById('start')
-const score = document.querySelector('.score')
-const points = document.querySelector('.points')
+const btn = document.querySelector('.level-buttons')
 
-
-startBtn.addEventListener('click', ()=> {
-  let timer = false
-  setTimeout(()=> timer = true, 3000)
+btn.addEventListener('click', (e)=> {
+  const scoreBoard = document.querySelector('.score-board')
+  setTimer(e)
   moles.forEach((mole) => {
-    mole.classList.remove('down')
     mole.classList.add('mole')
   })
-  score.innerHTML = 'Score: '
-  points.innerHTML = 0
-  window.setInterval(()=> {
-    getAMole()
-  }, 1000)
+  scoreBoard.style.visibility = 'visible'
 })
 
-function getAMole() {
-  let random = moles[Math.floor(Math.random() * moles.length)]
+function setTimer(e) {
+  let counter = 0
+  let timer = setInterval(()=> {
+    counter ++
+    if(counter === 20) {
+      clearInterval(timer)
+    }
+    getAMole(e)
+  }, 1000)
+}
+
+function getAMole(e) {
+  const button = e.target.value
+  const random = moles[Math.floor(Math.random() * moles.length)]
   random.classList.remove('mole')
   random.classList.add('up')
-  setTimeout(()=> reset(random), 1000)
+  button === "Easy Mode" ? setTimeout(()=> reset(random), 1000) : setTimeout(()=> reset(random), 500)
 }
 
 function reset(random) {
   random.classList.remove('up')
-  random.classList.add('down')
+  random.classList.add('mole')
 }
 
 moles.forEach((mole) => {
+  const points = document.querySelector('.points')
   mole.addEventListener('click', (e)=> {
-    if(e.screenY <= 450) {
+    if(e.screenY <= 363) {
       points.innerHTML++
       reset(mole);
     }
